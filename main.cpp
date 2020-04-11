17bc4c50f3c1b0213c89eddd5bac3a303459b7c1
yeom <yeom0331@naver.com>
2020-04-11 06:16:10 -0700

    Add Main.cpp

---
+++ main.cpp
@@ -0,0 +1,92 @@
+#include "pcap_test.h"
+#include <algorithm>
+using namespace std;
+
+void usage() {
+  printf("syntax: pcap-test <interface>\n");
+  printf("sample: pcap-test wlan0\n");
+}
+
+void print_MAC(const u_int8_t *mac) {
+    for(int i=0; i<6; i++) {
+        printf("%02x", mac[i]);
+        if(i<5) {
+        printf(":");
+        }
+    }
+    printf("\n");
+}
+
+void print_IP(const u_int8_t *ipaddr) {
+    for(int i=0; i<4; i++) {
+        printf("%d", ipaddr[i]);
+        if(i<3) {
+        printf(".");
+        }
+    }
+    printf("\n");
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        usage();
+        return -1;
+    }
+
+    char* dev = argv[1];
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+    if (handle == nullptr) {
+        fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
+        return -1;
+    }
+
+    while (true) {
+        struct pcap_pkthdr* header;
+        const u_char* packet;
+        int res = pcap_next_ex(handle, &header, &packet);
+        if (res == 0) continue;
+        if (res == -1 || res == -2) {
+            printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(handle));
+            break;
+        }
+        struct libnet_ethernet_hdr *eth;
+        struct libnet_ipv4_hdr *iph;
+        struct libnet_tcp_hdr *tcph;
+
+        eth = (struct libnet_ethernet_hdr *)packet;
+        u_int16_t ether_type = ntohs(eth->ether_type);
+
+        if(ether_type == ETHERTYPE_IP) {
+            iph = (struct libnet_ipv4_hdr *)(packet + sizeof(struct libnet_ethernet_hdr));
+            if(iph->ip_p == IPPROTO_TCP) {
+                printf("Src MAC = ");
+                print_MAC(eth->ether_shost);
+                printf("dst MAC = ");
+                print_MAC(eth->ether_dhost);
+
+                printf("Src IP = ");
+                print_IP(iph->ip_src);
+                printf("Dst IP = ");
+                print_IP(iph->ip_dst);
+
+                int ip_hl = (iph->ip_hl)*4;
+                int th_off = (tcph->th_x2)*4;
+
+                tcph = (struct libnet_tcp_hdr *)(packet + sizeof(struct libnet_ethernet_hdr) + ip_hl);
+
+                printf("Src PORT = %d\n", ntohs(tcph->th_sport));
+                printf("Dst PORT = %d\n", ntohs(tcph->th_dport));
+
+                int payload_len = ntohs(iph->ip_len) - ip_hl - th_off;
+                int print_len = min(payload_len, 16);
+                for(int i=0; i<print_len; i++) {
+                    printf("%02x ", packet[sizeof(struct libnet_ethernet_hdr) + ip_hl + th_off + i]);
+
+                }
+                printf("\n\n");
+            }
+        }
+    }
+    pcap_close(handle);
+}
---
+++ pcap_test.h
@@ -0,0 +1,131 @@
+#pragma once
+#ifndef PCAP_TEST_H
+#define PCAP_TEST_H
+#include <pcap.h>
+#include <arpa/inet.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <netinet/in.h>
+#define ETHER_ADDR_LEN 6
+
+struct libnet_ethernet_hdr
+{
+    u_int8_t  ether_dhost[ETHER_ADDR_LEN];/* destination ethernet address */
+    u_int8_t  ether_shost[ETHER_ADDR_LEN];/* source ethernet address */
+    u_int16_t ether_type;                 /* protocol */
+};
+
+#ifndef ETHERTYPE_PUP
+#define ETHERTYPE_PUP           0x0200  /* PUP protocol */
+#endif
+#ifndef ETHERTYPE_IP
+#define ETHERTYPE_IP            0x0800  /* IP protocol */
+#endif
+#ifndef ETHERTYPE_ARP
+#define ETHERTYPE_ARP           0x0806  /* addr. resolution protocol */
+#endif
+#ifndef ETHERTYPE_REVARP
+#define ETHERTYPE_REVARP        0x8035  /* reverse addr. resolution protocol */
+#endif
+#ifndef ETHERTYPE_VLAN
+#define ETHERTYPE_VLAN          0x8100  /* IEEE 802.1Q VLAN tagging */
+#endif
+#ifndef ETHERTYPE_EAP
+#define ETHERTYPE_EAP           0x888e  /* IEEE 802.1X EAP authentication */
+#endif
+#ifndef ETHERTYPE_MPLS
+#define ETHERTYPE_MPLS          0x8847  /* MPLS */
+#endif
+#ifndef ETHERTYPE_LOOPBACK
+#define ETHERTYPE_LOOPBACK      0x9000  /* used to test interfaces */
+#endif
+
+struct libnet_ipv4_hdr
+{
+    u_int8_t ip_hl:4,      /* header length */
+           ip_v:4;          /* version */
+#if (LIBNET_BIG_ENDIAN)
+    u_int8_t ip_v:4,       /* version */
+           ip_hl:4;        /* header length */
+#endif
+    u_int8_t ip_tos;       /* type of service */
+#ifndef IPTOS_LOWDELAY
+#define IPTOS_LOWDELAY      0x10
+#endif
+#ifndef IPTOS_THROUGHPUT
+#define IPTOS_THROUGHPUT    0x08
+#endif
+#ifndef IPTOS_RELIABILITY
+#define IPTOS_RELIABILITY   0x04
+#endif
+#ifndef IPTOS_LOWCOST
+#define IPTOS_LOWCOST       0x02
+#endif
+    u_int16_t ip_len;         /* total length */
+    u_int16_t ip_id;          /* identification */
+    u_int16_t ip_off;
+#ifndef IP_RF
+#define IP_RF 0x8000        /* reserved fragment flag */
+#endif
+#ifndef IP_DF
+#define IP_DF 0x4000        /* dont fragment flag */
+#endif
+#ifndef IP_MF
+#define IP_MF 0x2000        /* more fragments flag */
+#endif
+#ifndef IP_OFFMASK
+#define IP_OFFMASK 0x1fff   /* mask for fragmenting bits */
+#endif
+    u_int8_t ip_ttl;          /* time to live */
+    u_int8_t ip_p;            /* protocol */
+    u_int16_t ip_sum;         /* checksum */
+    u_int8_t ip_src[4];
+    u_int8_t ip_dst[4]; /* source and dest address */
+};
+
+struct libnet_tcp_hdr
+{
+    u_int16_t th_sport;       /* source port */
+    u_int16_t th_dport;       /* destination port */
+    u_int32_t th_seq;          /* sequence number */
+    u_int32_t th_ack;          /* acknowledgement number */
+
+    u_int8_t th_x2:4,         /* (unused) */
+           th_off:4;        /* data offset */
+
+#if (LIBNET_BIG_ENDIAN)
+    u_int8_t th_off:4,        /* data offset */
+           th_x2:4;         /* (unused) */
+#endif
+    u_int8_t  th_flags;       /* control flags */
+#ifndef TH_FIN
+#define TH_FIN    0x01      /* finished send data */
+#endif
+#ifndef TH_SYN
+#define TH_SYN    0x02      /* synchronize sequence numbers */
+#endif
+#ifndef TH_RST
+#define TH_RST    0x04      /* reset the connection */
+#endif
+#ifndef TH_PUSH
+#define TH_PUSH   0x08      /* push data to the app layer */
+#endif
+#ifndef TH_ACK
+#define TH_ACK    0x10      /* acknowledge */
+#endif
+#ifndef TH_URG
+#define TH_URG    0x20      /* urgent! */
+#endif
+#ifndef TH_ECE
+#define TH_ECE    0x40
+#endif
+#ifndef TH_CWR
+#define TH_CWR    0x80
+#endif
+    u_int16_t th_win;         /* window */
+    u_int16_t th_sum;         /* checksum */
+    u_int16_t th_urp;         /* urgent pointer */
+};
+
+#endif PCAP_TEST_H
